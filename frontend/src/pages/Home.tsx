cba68bf39c6f25ae24d056a93d43168cb74baa86
Jaeyong Lee <lee.j1533@gmail.com>
2023-07-07 23:46:52 -0400

    Add submission handlers for login and signup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,21 +5,28 @@ import {
     Text, 
     Container, 
     FormControl, 
-    FormLabel, Heading, 
+    FormLabel, 
+    Heading, 
     Input, 
     InputGroup, 
     InputRightElement 
 } from "@chakra-ui/react"
 import React from "react"
+import { useState } from "react"
 import { ColorModeSwitcher } from "../ColorModeSwitcher"
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Home = () => {
-    // const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        console.log(username, password)
+    }
 
     return (
         <div>
@@ -31,33 +38,40 @@ export const Home = () => {
                 <Box w='60%' textAlign="center">
                     <Heading textAlign="center">Welcome to EasyChat!</Heading>
                     <Text textAlign="center">Log In</Text>
-                    <FormControl>
-                        <FormLabel pt={7}>Username</FormLabel>
-                        <Input 
-                            placeholder='eg. chattymonkey123' 
-                            _placeholder={{ opacity: 1, color: 'gray.600' }}
-                            type='username' 
-                        />
 
-                        <FormLabel pt={7}>Password</FormLabel>
-                        <InputGroup size='md'>
-                            <Input
-                            pr='4.5rem'
-                            type={show ? 'text' : 'password'}
-                            placeholder='Enter password'
-                            _placeholder={{ opacity: 1, color: 'gray.600' }}
+                    <form onSubmit={handleSubmit}>
+                        <FormControl>
+                            <FormLabel pt={7}>Username</FormLabel>
+                            <Input 
+                                placeholder='eg. chattymonkey123' 
+                                _placeholder={{ opacity: 1, color: 'gray.600' }}
+                                type='username'
+                                onChange={(e) => setUsername(e.target.value)} 
+                                value={username}
                             />
-                            <InputRightElement width='4.5rem'>
-                            <Button h='1.75rem' size='sm' onClick={handleClick}>
-                                {show ? 'Hide' : 'Show'}
-                            </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
 
-                    <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline' mt={7}>
-                        Log in
-                    </Button>
+                            <FormLabel pt={7}>Password</FormLabel>
+                            <InputGroup size='md'>
+                                <Input
+                                pr='4.5rem'
+                                type={show ? 'text' : 'password'}
+                                placeholder='Enter password'
+                                _placeholder={{ opacity: 1, color: 'gray.600' }}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                value={password}
+                                />
+                                <InputRightElement width='4.5rem'>
+                                <Button h='1.75rem' size='sm' onClick={handleClick}>
+                                    {show ? 'Hide' : 'Show'}
+                                </Button>
+                                </InputRightElement>
+                            </InputGroup>
+
+                            <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline' type="submit" mt={7}>
+                                Log in
+                            </Button>
+                        </FormControl>
+                    </form>
 
                     <Text pt={7} textAlign="center" fontSize="sm">
                         New to EasyChat? Sign up {' '}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,4 +1,5 @@
-import { Box, 
+import { 
+    Box, 
     Button, 
     Link, 
     Text, 
@@ -11,15 +12,21 @@ import { Box,
     InputRightElement 
 } from "@chakra-ui/react"
 import React from "react"
+import { useState } from "react"
 import { ColorModeSwitcher } from "../ColorModeSwitcher"
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Signup = () => {
-    // const [username, setUsername] = useState('')
-    // const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        console.log(username, password)
+    }
 
     return (
         <div>
@@ -27,38 +34,44 @@ export const Signup = () => {
                 <ColorModeSwitcher />
             </Box>
 
-
             <Container h="80vh" maxW="2xl" centerContent display="flex" justifyContent="center" alignItems="center">
                 <Box w='60%' textAlign="center">
                     <Heading textAlign="center">Welcome to EasyChat!</Heading>
                     <Text textAlign="center">Sign Up</Text>
-                    <FormControl>
-                        <FormLabel pt={7}>Username</FormLabel>
-                        <Input 
-                            placeholder='eg. chattymonkey123' 
-                            _placeholder={{ opacity: 1, color: 'gray.600' }}
-                            type='username' 
-                        />
 
-                        <FormLabel pt={7}>Password</FormLabel>
-                        <InputGroup size='md'>
-                            <Input
-                            pr='4.5rem'
-                            type={show ? 'text' : 'password'}
-                            placeholder='Enter password'
-                            _placeholder={{ opacity: 1, color: 'gray.600' }}
+                    <form onSubmit={handleSubmit}>
+                        <FormControl>
+                            <FormLabel pt={7}>Username</FormLabel>
+                            <Input 
+                                placeholder='eg. chattymonkey123' 
+                                _placeholder={{ opacity: 1, color: 'gray.600' }}
+                                type='username' 
+                                onChange={(e) => setUsername(e.target.value)} 
+                                value={username}
                             />
-                            <InputRightElement width='4.5rem'>
-                            <Button h='1.75rem' size='sm' onClick={handleClick}>
-                                {show ? 'Hide' : 'Show'}
-                            </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
 
-                    <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline' mt={7}>
-                        Sign Up
-                    </Button>
+                            <FormLabel pt={7}>Password</FormLabel>
+                            <InputGroup size='md'>
+                                <Input
+                                pr='4.5rem'
+                                type={show ? 'text' : 'password'}
+                                placeholder='Enter password'
+                                _placeholder={{ opacity: 1, color: 'gray.600' }}
+                                onChange={(e) => setPassword(e.target.value)} 
+                                value={password}
+                                />
+                                <InputRightElement width='4.5rem'>
+                                <Button h='1.75rem' size='sm' onClick={handleClick}>
+                                    {show ? 'Hide' : 'Show'}
+                                </Button>
+                                </InputRightElement>
+                            </InputGroup>
+
+                            <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline' type="submit" mt={7}>
+                                Sign Up
+                            </Button>
+                        </FormControl>
+                    </form>
 
                     <Text pt={7} textAlign="center" fontSize="sm">
                         Already have an account? Log in{' '}
