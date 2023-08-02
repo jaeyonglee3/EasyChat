6e538e33b76a5d1acccf83f5769905081e448fdc
Jaeyong Lee <lee.j1533@gmail.com>
2023-08-02 14:54:22 -0400

    UI change to ChatArea

--- frontend/src/components/ChatArea.tsx
+++ frontend/src/components/ChatArea.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Text } from '@chakra-ui/react';
+import { Box, Center, useColorMode, Text } from '@chakra-ui/react';
 import { useFriendContext } from '../context/FriendContext';
 import { useEffect } from 'react';
 
@@ -6,11 +6,17 @@ const ChatArea = () => {
   const { selectedFriend } = useFriendContext();
   useEffect(() => {}, [selectedFriend]); 
 
+  // const { colorMode } = useColorMode();
+  // const colour1 = colorMode === "light" ? "gray.200" : "gray.800";
+
   return (
     <Box w="80vw" h="100vh" p="30px">
-      <Center h='100%' color='white'>
-        <Text>Select a conversation to get started :D You have selected: {selectedFriend && selectedFriend}</Text>
-      </Center>
+      {selectedFriend === null && (
+        <Center h='100%'>
+          <Text>Select a conversation to get started :D</Text>
+        </Center>
+      )}
+      <Text>{selectedFriend}</Text>
     </Box>
   );
 };
