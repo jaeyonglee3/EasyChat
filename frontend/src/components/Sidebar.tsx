750b69ca8b531bccc71c434162d9934e75ea527c
Jaeyong Lee <lee.j1533@gmail.com>
2023-08-02 10:05:19 -0400

    Add frontend for list user's friends

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,12 +1,13 @@
-import { Heading, Button, Box, Divider, useColorMode, VStack, Flex, Icon, HStack, Spacer, Stack } from "@chakra-ui/react"
+import { Heading, Button, Box, Divider, useColorMode, VStack, Flex, Icon, HStack, Spacer, Stack, Text } from "@chakra-ui/react"
 import { useLogout } from "../hooks/useLogout"
 import { ColorModeSwitcher } from "../ColorModeSwitcher"
 import AddFriendModal from "./AddFriendModal"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { CgProfile } from 'react-icons/cg'
 import { BiLogOutCircle } from 'react-icons/bi'
 
 export default function Sidebar() {
+    const [friends, setFriends] = useState([]);
     let username = ""
     const currUser = localStorage.getItem("user");
     const { logout } = useLogout()
@@ -15,8 +16,21 @@ export default function Sidebar() {
         const user = JSON.parse(currUser);
         username = user.username;
     }
-
-    // write a function to get and store a list of the current user's friends
+    
+    useEffect(() => {
+        const getFriends = async () => {
+            const response = await fetch(`/api/user/get-friends?username=${username}`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+              });
+        
+            const data = await response.json();
+            return data.friends; 
+        };
+        getFriends().then(friendsData => {
+            setFriends(friendsData);
+        });
+    }, [username]);
 
     const { colorMode } = useColorMode();
     const colour1 = colorMode === "light" ? "gray.200" : "gray.800";
@@ -35,8 +49,12 @@ export default function Sidebar() {
 
                     <VStack flex="1" justifyContent="flex-end"> 
 
-                        <Box bg={colour2} flex="1" w="100%" maxHeight="75vh" overflowY="auto" borderRadius="10px">
-                            
+                        <Box bg={colour2} flex="1" w="100%" maxHeight="75vh" overflowY="auto" borderRadius="10px" p="15px" alignContent="center">
+                            <VStack align="stretch"> {/* Wrap the Box with VStack */}
+                                {Array.isArray(friends) && friends.map((friend, index) => (
+                                    <Button key={index} borderRadius="10px">{friend}</Button> 
+                                ))}
+                            </VStack>
                         </Box>
 
                         <Divider mb={5}/>
